f684f8467a5b64af8758db1ecf77d89f915f45e1
John Doe <johndoe@example.com>
2019-08-28 17:39:59 +0100

    new commit

--- App.java
+++ App.java
@@ -11,7 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-    	for (int i =0; i< 10; i++)
+    	for (int i =0; i< 5; i++)
     	{
     		System.out.println(new App().getMessage());
     	}
